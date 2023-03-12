0a5276e1c7321097b633349a06ae20f966bd2a54
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-03-12 18:22:15 +0900

    FIX: (user) 인바운드 포트 구현한 서비스를 유저 모듈에 등록

--- account-book-backend/src/user/user.module.ts
+++ account-book-backend/src/user/user.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
+import { CREATE_USER_INBOUND_PORT } from './inbound-port/create-user.inbound-port';
+import { CreateUserService } from './service/create-user.service';
 
 @Module({
   imports: [],
   controllers: [UserController],
-  providers: [],
+  providers: [
+    {
+      provide: CREATE_USER_INBOUND_PORT,
+      useClass: CreateUserService,
+    },
+  ],
 })
 export class UserModule {}
