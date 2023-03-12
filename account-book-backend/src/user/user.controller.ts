2e74f2059a2247316911c223671971da2c6c9c85
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-03-12 18:22:32 +0900

    FIX: (user) 계정 생성용 엔드포인트 셋팅

--- account-book-backend/src/user/user.controller.ts
+++ account-book-backend/src/user/user.controller.ts
@@ -1,9 +1,27 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post, Query } from '@nestjs/common';
+import {
+  CREATE_USER_INBOUND_PORT,
+  CreateUserInboundPort,
+  CreateUserInboundPortInputDto,
+  CreateUserInboundPortOutputDto,
+} from './inbound-port/create-user.inbound-port';
 
 @Controller('user')
 export class UserController {
+  constructor(
+    @Inject(CREATE_USER_INBOUND_PORT)
+    private readonly createUserInboundPort: CreateUserInboundPort,
+  ) {}
+
   @Get('test')
   async test(@Query() queryString: any) {
     return queryString;
+  }
+
+  @Post('create')
+  async create(
+    @Body() body: CreateUserInboundPortInputDto,
+  ): Promise<CreateUserInboundPortOutputDto> {
+    return this.createUserInboundPort.execute(body);
   }
 }
