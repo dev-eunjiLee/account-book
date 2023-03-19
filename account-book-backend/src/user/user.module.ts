32a8ba71a4096d750104b7fc63ad9b721e98ae83
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-03-19 18:38:56 +0900

    FIX: (user) 유저 생성 outbound 연결

--- account-book-backend/src/user/user.module.ts
+++ account-book-backend/src/user/user.module.ts
@@ -4,6 +4,8 @@ import { CREATE_USER_INBOUND_PORT } from './inbound-port/create-user.inbound-por
 import { CreateUserService } from './service/create-user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
+import { CREATE_USER_OUTBOUND_PORT } from './outbound-port/create-user.outbound-port';
+import { CreateUserOutboundAdapter } from './outbound-adapter/create-user.outbound-adapter';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
@@ -12,6 +14,10 @@ import { User } from './entity/user.entity';
     {
       provide: CREATE_USER_INBOUND_PORT,
       useClass: CreateUserService,
+    },
+    {
+      provide: CREATE_USER_OUTBOUND_PORT,
+      useClass: CreateUserOutboundAdapter,
     },
   ],
 })
