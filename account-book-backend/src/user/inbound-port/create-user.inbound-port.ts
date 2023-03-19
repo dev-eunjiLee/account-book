2123ccad4569eef4823848c7efc5cb9122d91b10
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-03-19 18:12:50 +0900

    FIX: (user) 엔터티에서 input dto 설정하도록 수정

--- account-book-backend/src/user/inbound-port/create-user.inbound-port.ts
+++ account-book-backend/src/user/inbound-port/create-user.inbound-port.ts
@@ -1,9 +1,8 @@
+import { User } from '../entity/user.entity';
+
 export const CREATE_USER_INBOUND_PORT = 'CREATE_USER_INBOUND_PORT';
 
-export type CreateUserInboundPortInputDto = {
-  id: string;
-  password: string;
-};
+export type CreateUserInboundPortInputDto = Pick<User, 'password' | 'id'>;
 export type CreateUserInboundPortOutputDto = void;
 
 export interface CreateUserInboundPort {
