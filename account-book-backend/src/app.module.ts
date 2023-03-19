aeebceef7dbe5c6e4110e78457edce8e9d82574f
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-03-19 18:03:59 +0900

    FIX: (db) typeorm 셋팅

--- account-book-backend/src/app.module.ts
+++ account-book-backend/src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountBookModule } from './account-book/account-book.module';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, AccountBookModule],
+  imports: [
+    UserModule,
+    AccountBookModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '34.82.23.63',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'mysql',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
