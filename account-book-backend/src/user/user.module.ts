46638fe2c6c6df7482b1b42678cb296da9618fb4
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-03-19 18:12:28 +0900

    FIX: (user) TypeOrmModule 등록

--- account-book-backend/src/user/user.module.ts
+++ account-book-backend/src/user/user.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { CREATE_USER_INBOUND_PORT } from './inbound-port/create-user.inbound-port';
 import { CreateUserService } from './service/create-user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [
     {
